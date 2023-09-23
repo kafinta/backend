b5e060c4eb29e2fafcebe4a99dbe32460cd9415d
Qaudri <aquadirmuhammad@gmail.com>
2023-09-23 12:18:48 +0100

    user functions in progress

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Cloudinary\Cloudinary;
+
+class UserController extends Controller
+{
+    public function signup(Request $request){
+
+        $validator = $this->validateNewUser();
+
+        if ($validator->fails()) {
+            return response()-> json([
+                'status' => 'fail',
+                'message' => $validator->messages(),
+            ], 422);
+        }
+
+        $createUser = User::create([
+            'email' => $request->email,
+            'username' => $request->username,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $data = [
+            'token' => $createUser->createToken('UserAuthToken')->plainTextToken,
+            'token_type' => 'Bearer'
+        ];
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'New user created successfully',
+            'data' => $data
+        ], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // Attempt to authenticate the user
+
+        if (Auth::attempt(['email'|| 'username' => $request->email, 'password' => $request->password])) {
+            $data = [
+                'token' => auth()->user()->createToken('UserAuthToken')->plainTextToken,
+                'token_type' => 'Bearer',
+                'user' => Auth::user()
+            ];
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User authentication successful',
+                'data' => $data
+            ], 200);
+        }
+
+        return response()-> json([
+            'status' => 'fail',
+            'message' => 'Invalid credentials',
+        ], 422);
+    }
+
+    public function getAccountDetails(){
+        $data = Auth::user();
+        return $data;
+    }
+
+    public function updateProfile(Request $request, Cloudinary $cloudinary, $id)
+    {
+        $user = Auth::user();
+
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'is_seller' => 'boolean', // Assuming is_seller is a boolean field
+            'profile_picture' => 'image|mimes:jpeg,png,jpg|max:2048', // Adjust validation rules for the profile picture
+        ]);
+
+        // Handle profile picture upload
+        if ($request->hasFile('profile_picture')) {
+            $file = $request->file('profile_picture');
+            $uploadedFile = $cloudinary->uploadApi()->upload($file->getPathname());
+
+            // Save the Cloudinary public ID to the user's profile_picture column
+            $user->profile_picture = $uploadedFile['public_id'];
+        }
+
+        $user->update($validatedData);
+        
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Profile updated successfully'
+        ]);
+
+    }
+
+    public function validateNewUser()
+    {
+        return Validator::make(request()->all(), [
+            'email' => 'required|unique:users',
+            'username' => 'required|unique:users',
+            'password' => 'required|min:8'
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('/user/auth/register', 'UserController@signup');
+Route::post('/user/auth/login', 'UserController@login');
+Route::prefix('user')->middleware('auth:users-api')->group(function() {
+    Route::get('/account', 'UserController@getAccountDetails');
+
+});
+
