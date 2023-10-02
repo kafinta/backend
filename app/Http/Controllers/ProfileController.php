27781f94795b722c682a51a2906e1c6f45aa2005
Qaudri <aquadirmuhammad@gmail.com>
2023-10-02 10:48:31 +0100

    chore: profile update implementation in progress

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
-use Cloudinary\Cloudinary;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 
 class ProfileController extends Controller
 {
@@ -47,28 +47,63 @@ class ProfileController extends Controller
 
     public function updateProfile(Request $request, Cloudinary $cloudinary)
     {
+        $user = auth()->user();
+    
         $validatedData = $request->validate([
             'first_name' => 'required|string|max:255',
             'last_name' => 'required|string|max:255',
             'biography' => 'nullable|string',
-            'is_seller' => 'boolean', // Assuming is_seller is a boolean field
-            'profile_picture' => 'image|mimes:jpeg,png,jpg|max:2048', // Adjust validation rules for the profile picture
+            'is_seller' => 'nullable|boolean',
+            'profile_picture' => 'image|mimes:jpeg,png,jpg|max:2048',
         ]);
-
+    
         // Handle profile picture upload
         if ($request->hasFile('profile_picture')) {
             $file = $request->file('profile_picture');
-            $uploadedFile = $cloudinary->uploadApi()->upload($file->getPathname());
-
-            // Save the Cloudinary public ID to the profiles's profile_picture column
-            $profile->profile_picture = $uploadedFile['public_id'];
+    
+            // Check if the file exists
+            if (!file_exists($file->getPathname())) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'The profile picture file does not exist.',
+                ]);
+            }
+    
+            // Check if the file is empty
+            if (filesize($file->getPathname()) === 0) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'The profile picture file is empty.',
+                ]);
+            }
+    
+            try {
+                // Upload the file to Cloudinary
+                $uploadedFile = $cloudinary->uploadApi()->upload($file->getPathname());
+                // $uploadedFileUrl = Cloudinary::upload($file->getRealPath())->getSecurePath();
+    
+                // Save the Cloudinary public ID to the profiles's profile_picture column
+                $user->profile->profile_picture = $uploadedFileUrl['public_id'];
+            } catch (CloudinaryApiException $e) {
+                // Handle the Cloudinary API error here
+                return response()->json([
+                    'status' => 'error',
+                    'message' => $e->getHttpStatusCode() . ': ' . $e->getMessage(),
+                ]);
+            } catch (Exception $e) {
+                // Handle the general error here
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Image upload failed',
+                ]);
+            }
         }
-
-        $profile->update($validatedData);
-
+    
+        $user->profile->update($validatedData);
+    
         return response()->json([
             'status' => 'success',
-            'message' => 'Profile updated successfully'
+            'message' => 'Profile updated successfully',
         ]);
     }
 }
