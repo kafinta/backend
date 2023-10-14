5028ba03e9006f161dbb3ff7efa2c39623ed74c8
Qaudri <aquadirmuhammad@gmail.com>
2023-10-14 18:50:42 +0100

    product models and relationships defined

---
+++ database/migrations/2023_10_14_170606_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price', 10, 2);
+            $table->string('currency_code', 3); 
+            $table->string('style');
+            $table->string('material');
+            $table->string('image');
+            $table->unsignedBigInteger('subcategory_id');
+            $table->foreign('room_id')->references('id')->on('subcategories');
+            $table->timestamps();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
