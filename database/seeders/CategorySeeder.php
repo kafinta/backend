9604a29e657e906a0ef27a4f794d242970a09113
Quadir <aquadirmuhammad@gmail.com>
2024-04-06 14:32:15 +0100

    categories seeded with images

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use Illuminate\Support\Facades\Storage;
 
 class CategorySeeder extends Seeder
 {
@@ -15,59 +16,74 @@ class CategorySeeder extends Seeder
     {
         Category::create([
             'name' => 'Furniture',
-            'description' => 'Chairs, tables, beds, etc...'
+            'description' => 'Chairs, tables, beds, etc...',
+            'image' => Storage::url('/images/categories/furniture.jpg')
         ]);
         Category::create([
             'name' => 'Lighting',
-            'description' => 'Lamps, bulbs, etc...'
+            'description' => 'Lamps, bulbs, etc...',
+            'image' => Storage::url('/images/categories/lighting.jpg')
         ]);
         Category::create([
             'name' => 'Décor',
-            'description' => 'Wall art, rugs, mirrors, etc...'
+            'description' => 'Wall art, rugs, mirrors, etc...',
+            'image' => Storage::url('/images/categories/decor.jpg')
         ]);
         Category::create([
             'name' => 'Entertainment',
-            'description' => 'TVs, computers, speakers, etc...'
+            'description' => 'TVs, computers, speakers, etc...',
+            'image' => Storage::url('/images/categories/entertainment.jpg')
+
         ]);
         Category::create([
             'name' => 'Fabrics',
-            'description' => 'Bedding, curtains, towels, etc...'
+            'description' => 'Bedding, curtains, towels, etc...',
+            'image' => Storage::url('/images/categories/fabrics.jpg')
         ]);
         Category::create([
             'name' => 'Appliances',
-            'description' => 'Refrigerators, dishwashers, etc...'
+            'description' => 'Refrigerators, dishwashers, etc...',
+            'image' => Storage::url('/images/categories/appliances.jpg')
         ]);
         Category::create([
             'name' => 'Kitchenware',
-            'description' => 'Utensils, cookware, dining sets, etc...'
+            'description' => 'Utensils, cookware, dining sets, etc...',
+            'image' => Storage::url('/images/categories/kitchenware.jpg')
         ]);
         Category::create([
             'name' => 'Plumbing Fixtures',
-            'description' => 'Sinks, faucets, bathtubs, etc...'
+            'description' => 'Sinks, faucets, bathtubs, etc...',
+            'image' => Storage::url('/images/categories/plumbing.jpeg'),
         ]);
         Category::create([
             'name' => 'Home office',
-            'description' => 'Chairs, computers, printers, etc...'
+            'description' => 'Chairs, computers, printers, etc...',
+            'image' => Storage::url('/images/categories/office.jpg')
         ]);
         Category::create([
             'name' => 'Storage solutions',
-            'description' => 'Shelves, cabinets, baskets, etc...'
+            'description' => 'Shelves, cabinets, baskets, etc...',
+            'image' => Storage::url('/images/categories/storage.png')
         ]);
         Category::create([
             'name' => 'Outdoor & Gardening',
-            'description' => 'Patio furniture, grills, garden tools, etc...'
+            'description' => 'Patio furniture, grills, garden tools, etc...',
+            'image' => Storage::url('/images/categories/gardening.jpg')
         ]);
         Category::create([
             'name' => 'Hardware & Tools',
-            'description' => 'Hammers, screwdrivers, cleaning supplies, etc...'
+            'description' => 'Hammers, screwdrivers, cleaning supplies, etc...',
+            'image' => Storage::url('/images/categories/hardware.jpg')
         ]);
         Category::create([
             'name' => 'Home Improvement',
-            'description' => 'Doors, blinds, tiles, etc...'
+            'description' => 'Doors, blinds, tiles, etc...',
+            'image' => Storage::url('/images/categories/improvement.jpeg')
         ]);
         Category::create([
             'name' => 'Personal Care & Hygiene',
-            'description' => 'Toiletries, grooming tools, etc...'
+            'description' => 'Toiletries, grooming tools, etc...',
+            'image' => Storage::url('/images/categories/hygiene.jpg')
         ]);
     }
 }
