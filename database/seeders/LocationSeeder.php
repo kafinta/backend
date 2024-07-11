7e8bc7d107aff9bfa073033a64e7e586fba1f91b
Quadir <aquadirmuhammad@gmail.com>
2024-07-11 14:54:35 +0100

    database test seeded

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -19,14 +19,13 @@ class LocationSeeder extends Seeder
             [
                 'name' => 'Bedroom',
                 'image' => '/images/locations/bedroom.jpg'
-    
             ],
             [
                 'name' => 'Kitchen',
                 'image' => '/images/locations/kitchen.jpg'
             ],
             [
-                'name' => 'Dining',
+                'name' => 'Dining Room',
                 'image' => '/images/locations/dining.jpg'
             ],
             [
@@ -34,11 +33,11 @@ class LocationSeeder extends Seeder
                 'image' => '/images/locations/bathroom.jpg'
             ],
             [
-                'name' => 'Hallway',
+                'name' => 'Entryway & Hallway',
                 'image' => '/images/locations/hallway.jpg'
             ],
             [
-                'name' => 'Closet',
+                'name' => 'Closet & Storage',
                 'image' => '/images/locations/closet.jpg'
             ],
             [
@@ -46,7 +45,7 @@ class LocationSeeder extends Seeder
                 'image' => '/images/locations/office.jpg'
             ],
             [
-                'name' => 'Kids Room',
+                'name' => 'Children\'s Room',
                 'image' => '/images/locations/nursery.jpg'
             ],
             [
@@ -58,16 +57,12 @@ class LocationSeeder extends Seeder
                 'image' => '/images/locations/porch.jpg'
             ],
             [
-                'name' => 'Deck',
+                'name' => 'Deck & Patio',
                 'image' => '/images/locations/deck.jpeg'
             ],
             [
                 'name' => 'Pool Area',
                 'image' => '/images/locations/pool.jpg'
-            ],
-            [
-                'name' => 'Patio',
-                'image' => '/images/locations/patio.png'
             ],
             [
                 'name' => 'Basement',
@@ -76,6 +71,10 @@ class LocationSeeder extends Seeder
             [
                 'name' => 'Attic',
                 'image' => '/images/locations/attic.jpg'
+            ],
+            [
+                'name' => 'Game Room',
+                'image' => '/images/locations/game_room.webp'
             ],
             [
                 'name' => 'Utility Room',
@@ -94,12 +93,8 @@ class LocationSeeder extends Seeder
                 'image' => '/images/locations/driveway.jpg'
             ],
             [
-                'name' => 'Exterior',
+                'name' => 'Outdoor & Exterior',
                 'image' => '/images/locations/exterior.jpg'
-            ],
-            [
-                'name' => 'Outdoor',
-                'image' => '/images/locations/outdoor.jpg'
             ],
         ];
 
