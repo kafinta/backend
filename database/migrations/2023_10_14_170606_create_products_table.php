2806f18352fc17e3ffeb2c555a291be202642ea5
Quadir <aquadirmuhammad@gmail.com>
2024-12-12 17:30:00 +0100

    auth implementation updated

--- database/migrations/2023_10_14_170606_create_products_table.php
+++ database/migrations/2023_10_14_170606_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description');
             $table->decimal('price', 8, 2);
-            // $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('subcategory_id')->constrained()->onDelete('cascade');
             $table->boolean('is_active')->default(true);
             $table->timestamps();
@@ -29,5 +29,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('products');
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
     }
 };
