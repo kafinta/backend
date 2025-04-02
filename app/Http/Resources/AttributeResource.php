703cb99fcc333c5b7a95a782ab8c1ba1d7906f6c
Quadir <aquadirmuhammad@gmail.com>
2025-04-02 16:22:30 +0100

    chore: new resources updated

---
+++ app/Http/Resources/AttributeResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AttributeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'type' => $this->type,
+            'is_variant_generator' => $this->is_variant_generator,
+            'values' => AttributeValueResource::collection($this->whenLoaded('values'))
+        ];
+    }
+}
