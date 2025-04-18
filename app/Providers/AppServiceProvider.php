067541b5b805b1eb5e13acfeff92e9cbcfb3d915
Quadir <aquadirmuhammad@gmail.com>
2025-04-18 21:57:15 +0100

    feat: consolidate multistep form services into AppServiceProvider and remove FormServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
+use App\Services\FileService;
+use App\Services\MultistepFormService;
+use App\Services\MultistepFormServiceV2;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +17,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Register services as singletons
+
+        // File handling service
+        $this->app->singleton(FileService::class, function ($app) {
+            return new FileService();
+        });
+
+        // Form services
+        $this->app->singleton(MultistepFormService::class, function ($app) {
+            return new MultistepFormService();
+        });
+
+        $this->app->singleton(MultistepFormServiceV2::class, function ($app) {
+            return new MultistepFormServiceV2();
+        });
     }
 
     /**
