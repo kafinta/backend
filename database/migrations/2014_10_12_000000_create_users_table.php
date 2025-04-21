0d40ade21cf180586f8e59803fce6185a3acd26d
Quadir <aquadirmuhammad@gmail.com>
2025-04-21 18:49:20 +0100

    refactor: make username unique and ensure phone_number and profile_picture are nullable in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique();
-            $table->string('username');
+            $table->string('username')->unique();
+            $table->string('phone_number')->nullable();
+            $table->string('profile_picture')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
