f51175a2f3f9a4677f4571c30aa362c63bc05857
Quadir <aquadirmuhammad@gmail.com>
2025-04-23 22:06:42 +0100

    feat: update CartItem model to include variant_id in fillable attributes and add variant relationship

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -9,10 +9,20 @@ class CartItem extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['cart_id', 'product_id', 'quantity'];
+    protected $fillable = ['cart_id', 'product_id', 'variant_id', 'quantity'];
+
+    public function cart()
+    {
+        return $this->belongsTo(Cart::class);
+    }
 
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function variant()
+    {
+        return $this->belongsTo(Variant::class);
     }
 }
