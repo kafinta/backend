7ab41cbb0c539104e66abfcc772454f2fcad78b3
Quadir <aquadirmuhammad@gmail.com>
2025-05-09 17:42:51 +0100

    refactor: remove timestamp fields from UserAccountResource output

--- app/Http/Resources/UserAccountResource.php
+++ app/Http/Resources/UserAccountResource.php
@@ -14,6 +14,11 @@ class UserAccountResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        $data = parent::toArray($request);
+        // Remove timestamp fields
+        unset($data['created_at']);
+        unset($data['updated_at']);
+
+        return $data;
     }
 }
