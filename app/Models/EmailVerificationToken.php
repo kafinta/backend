ec1c58406242458b64f95c2a9c01b79c3ff14274
Quadir <aquadirmuhammad@gmail.com>
2025-05-15 16:12:24 +0100

    feat: add EmailVerificationToken model for handling email verification tokens

---
+++ app/Models/EmailVerificationToken.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EmailVerificationToken extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'email',
+        'token',
+        'verification_code',
+        'expires_at',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'expires_at' => 'datetime',
+    ];
+
+    /**
+     * Get the user that owns the token.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Check if the token is expired.
+     *
+     * @return bool
+     */
+    public function isExpired()
+    {
+        return $this->expires_at->isPast();
+    }
+}
