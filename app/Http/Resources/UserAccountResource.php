8d3bbe95e6ab129bd8c5dc00bcb17e6a37c70afe
Quadir <aquadirmuhammad@gmail.com>
2025-05-24 14:33:02 +0100

    refactor: simplify UserAccountResource to directly return user data without timestamps

--- app/Http/Resources/UserAccountResource.php
+++ app/Http/Resources/UserAccountResource.php
@@ -14,11 +14,24 @@ class UserAccountResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $data = parent::toArray($request);
-        // Remove timestamp fields
-        unset($data['created_at']);
-        unset($data['updated_at']);
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            'email' => $this->email,
+            'phone_number' => $this->phone_number,
+            'profile_picture' => $this->profile_picture,
+            'email_verified_at' => $this->email_verified_at,
 
-        return $data;
+            // OAuth information
+            'oauth_provider' => $this->provider,
+            'oauth_provider_display' => $this->getProviderDisplayName(),
+            'is_oauth_user' => $this->isOAuthUser(),
+            'has_password' => $this->hasPassword(),
+
+            // Role information
+            'roles' => $this->roles->pluck('slug'),
+            'is_seller' => $this->isSeller(),
+            'is_admin' => $this->isAdmin(),
+        ];
     }
 }
